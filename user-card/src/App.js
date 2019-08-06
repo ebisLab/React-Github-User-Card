07439bf23082fe4346cec93c8f0913aae86df760
ebisLab <ebaiden@techie.com>
2019-08-06 16:28:04 -0400

    converted to class component

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
+
+class App extends React.Component {
+  constructor(){
+    super();
+  this.state = {
+    users: [],
+    userTypes: 'users'
+  }
+  }
+
+
+  render(){
+
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Populate Users Here</h1>
     </div>
   );
 }
+}
 
 export default App;
+
