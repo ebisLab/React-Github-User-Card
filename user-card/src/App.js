c0f7d8c34c10e6ebc5ddfa03f58c9470172ff95d
ebisLab <ebaiden@techie.com>
2019-08-06 19:31:14 -0400

    button

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -14,22 +14,15 @@ class App extends React.Component {
   componentDidMount(){
     this.fetchUsers();
   }
+
 //1. Fetch 
   fetchUsers = () =>{
     
     fetch(`https://api.github.com/users`)
     .then(res=>  res.json())
-      //{
-      //console.log('res', res.json())
-      
-      // return console.log('There', res.json())
-
-   // })
     .then(folks => { 
       this.setState({data: folks})
       return console.log('There', folks)})
-      
-  //  return console.log('There')
    
    .catch(err => {
      console.log(err);
@@ -40,10 +33,24 @@ class App extends React.Component {
 
    return (
     <div className="App">
+      <div className="inside">
     <h1>Populate Users Here</h1>
     {this.state.data.map(peep =>{
-      return <p>{peep.login}</p>
+      return <div className="cards">
+        <div className="long">
+        <div className="avi">
+          <img className="avicircle"src={peep.avatar_url} />
+        </div>
+        <div>
+        <h3>{peep.login}</h3>
+      <p>
+        <div className="btn" onClick={peep.url}>More</div>
+      </p>
+      </div>
+      </div>
+      </div>
     })}
+    </div>
     </div>
   );
 }
