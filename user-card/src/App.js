d394d0158fcae1d5f458f67e0f23c3588c03f8d6
ebisLab <ebaiden@techie.com>
2019-08-06 18:17:07 -0400

    fetch users from api

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -6,17 +6,44 @@ class App extends React.Component {
   constructor(){
     super();
   this.state = {
-    users: [],
-    userTypes: 'users'
-  }
+    data: []
+  };
   }
 
+  //2.set to state
+  componentDidMount(){
+    this.fetchUsers();
+  }
+//1. Fetch 
+  fetchUsers = () =>{
+    
+    fetch(`https://api.github.com/users`)
+    .then(res=>  res.json())
+      //{
+      //console.log('res', res.json())
+      
+      // return console.log('There', res.json())
 
+   // })
+    .then(folks => { 
+      this.setState({data: folks})
+      return console.log('There', folks)})
+      
+  //  return console.log('There')
+   
+   .catch(err => {
+     console.log(err);
+   })
+  }
+  
   render(){
 
    return (
     <div className="App">
     <h1>Populate Users Here</h1>
+    {this.state.data.map(peep =>{
+      return <p>{peep.login}</p>
+    })}
     </div>
   );
 }
